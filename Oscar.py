0632cbcaf121e121bf9895017364d93a3563539e
obabilonia <oscar-babilonia@hotmail.com>
2018-11-23 18:15:16 -0200

    Add files via upload

--- Oscar.py
+++ Oscar.py
@@ -4,6 +4,19 @@ import pandas as pd
 import numpy as np
 from unidecode import unidecode
 
+def O_print_full(x):
+    pd.set_option('display.max_rows', len(x))
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 2000)
+    pd.set_option('display.float_format', '{:20,.2f}'.format)
+    pd.set_option('display.max_colwidth', -1)
+    print(x)
+    pd.reset_option('display.max_rows')
+    pd.reset_option('display.max_columns')
+    pd.reset_option('display.width')
+    pd.reset_option('display.float_format')
+    pd.reset_option('display.max_colwidth')
+
 def O_word_frequency(df_text, s, idioma = "portuguese"):
     """
     Implement a function O_word_frequency() in Python that takes as input
